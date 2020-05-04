ba6d62d2daa86e3bae142dc60818da496d2b0136
Harshal Patel <patelharshu6699@gmail.com>
2020-05-05 04:14:31 +0530

    Initial commit

---
+++ src/test/java/com/harshal/todo/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshal.todo.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
